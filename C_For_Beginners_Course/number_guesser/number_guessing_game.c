cb2d85279f32b2d08ac554191c58a157023c5a20
Jeremy Yandell <jereyand@gmail.com>
2020-04-16 22:42:24 -0700

    number game is working

--- C_For_Beginners_Course/number_guesser/number_guessing_game.c
+++ C_For_Beginners_Course/number_guesser/number_guessing_game.c
@@ -29,5 +29,28 @@ int main()
         scanf(" %c", &ready);
     } while (ready != 'Y');
 
+    // Game begins
+    for (int i = tries; i > 0; --i) {
+        printf("You have %d tries left.\n", i);
+        printf("Enter a guess: ");
+        scanf("%d", &guess);
+        if (guess == randomNumber) {
+            success = 1;
+            break;
+        } else {
+            printf("Sorry, %d is wrong. ", guess);
+            if (guess < randomNumber)
+                printf("My number is more than that.\n");
+            else
+                printf("My number is less than that.\n");
+        }
+    }
+    
+    if (success) {
+        printf("Congrats you are champion.\n");
+    } else {
+        printf("You are a loser and should feel shame.\n");
+    }
+
     return 0;
 }
