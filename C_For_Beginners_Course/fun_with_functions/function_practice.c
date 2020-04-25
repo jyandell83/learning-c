eb952a9e6e1bc73eed1a196a3c6411e3f6c4e663
Jeremy Yandell <jereyand@gmail.com>
2020-04-24 22:03:06 -0700

    find absolute value function works

--- C_For_Beginners_Course/fun_with_functions/function_practice.c
+++ C_For_Beginners_Course/fun_with_functions/function_practice.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
 
-
-
-
-
-
-
 unsigned int findGCD (unsigned int num1, unsigned int num2) {
    unsigned int result = 1;
    unsigned int least = num1 > num2 ? num2 : num1;
@@ -19,10 +13,11 @@ unsigned int findGCD (unsigned int num1, unsigned int num2) {
    return result;
 }
 
-/*
-float findAbsValue (float value) {
+double findAbsValue (double value) {
+    return value >= 0 ? value : value * -1;
 }
 
+/*
 float findSqRoot (float num) {
 }
 */
@@ -30,6 +25,12 @@ int main()
 {
     printf("GCD of 8 and 12 is %d\n", findGCD(8, 12));
     printf("GCD of 24 and 36 is %d\n", findGCD(24, 36));
+
+    
+    printf("The absolute value of 0 is %lf\n", findAbsValue(0.0));
+    printf("The absolute value of 10.2403 is %lf\n", findAbsValue(10.2403));
+    printf("The absolute value of -2000.034 is %lf\n", findAbsValue(-2000.034));
+    printf("The absolute value of -100 is %lf\n", findAbsValue(-100));
 
     return 0;
 }
