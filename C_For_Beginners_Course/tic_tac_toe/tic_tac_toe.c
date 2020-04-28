767fa38bba95236a0149309948cdf62824aa1044
Jeremy Yandell <jereyand@gmail.com>
2020-04-27 21:21:41 -0700

    check winner is working for rows

--- C_For_Beginners_Course/tic_tac_toe/tic_tac_toe.c
+++ C_For_Beginners_Course/tic_tac_toe/tic_tac_toe.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void renderBoard( char arr[9]) {
 
@@ -20,12 +21,39 @@ void renderBoard( char arr[9]) {
     printf("   |   |   \n\n");
 }
 
+bool checkForWin( char arr[9] ) {
 
+    bool threeInARow = 0;
+
+    //check rows
+    
+    if (arr[0] == arr[1] && arr[0] == arr[2])
+        threeInARow = 1;
+
+    if (arr[3] == arr[4] && arr[3] == arr[5])
+        threeInARow = 1;
+
+    if (arr[6] == arr[7] && arr[6] == arr[8])
+        threeInARow = 1;
+    
+    //check columns
+    //check diagonals
+
+    return threeInARow;
+}
 
 int main()
 {
     char gameState[9] = "123456789";
 
     renderBoard(gameState);
+
+    gameState[0] = 'X';
+    gameState[1] = 'X';
+    gameState[2] = 'X';
+
+    if (checkForWin(gameState))
+        printf("winner");
+
     return 0;
 }
