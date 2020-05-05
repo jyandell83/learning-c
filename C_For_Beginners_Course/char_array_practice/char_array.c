a28afd51c21e6b11168c03a7d3baba34c994ad23
Jeremy Yandell <jereyand@gmail.com>
2020-05-05 11:40:43 -0700

    function for couting characters in string added

--- C_For_Beginners_Course/char_array_practice/char_array.c
+++ C_For_Beginners_Course/char_array_practice/char_array.c
@@ -3,10 +3,27 @@
 
 // Takes string as input and calculates the number of chars
 
+int determineCharNumberInString (char inputStr[]) {
+
+    int countOfChars = 0;
+
+    for (int i = 0; inputStr[i] != '\0'; ++i)
+        countOfChars++;
+
+    return countOfChars;
+}
+
 // Concatenates 2 strings
 
 // Determines if two strings are equal
 int main()
 {
+    char test[20]; 
+
+    printf("Please enter a word (under 20 letters) for analysis: ");
+    scanf("%s", test);
+
+    printf("%s contains %d characters\n", test, determineCharNumberInString(test));
+
     return 0;
 }
