1175854289b77db51c542dae08c75a168b5981f4
Jeremy Yandell <jereyand@gmail.com>
2020-05-05 10:37:33 -0700

    initial pseudocode for char array

---
+++ C_For_Beginners_Course/char_array_practice/char_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Playing with some functions to help cement knowledge of char arrays / strings
+
+// Takes string as input and calculates the number of chars
+
+// Concatenates 2 strings
+
+// Determines if two strings are equal
+int main()
+{
+    return 0;
+}
