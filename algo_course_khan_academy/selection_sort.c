204149bfcf3cefc39ce87f2b3821d43f1f3cab64
Jeremy Yandell <jereyand@gmail.com>
2020-06-09 21:57:02 -0700

    indexOfMinimum is working

--- algo_course_khan_academy/selection_sort.c
+++ algo_course_khan_academy/selection_sort.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
-void swap(int array[10], int firstIndex, int secondIndex){
+void swap(int array[11], int firstIndex, int secondIndex){
     int temp = array[firstIndex];
     array[firstIndex] = array[secondIndex];
     array[secondIndex] = temp;
 }
 
+int indexOfMinimum(int array[11], int startIndex) {
+    int minValue = array[startIndex];
+    int minIndex = startIndex;
+
+    for (int i = minIndex + 1; array[i] != '\0'; ++i) {
+        if (array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    }   
+
+    return minIndex;
+}
+
 int main()
 {
-    int testArray[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int testArray[11] = {121, 2, 33, 554, 5, 76, 7, 78, 9, 10};
 
     int i = 0, j = 0;
 
@@ -18,6 +32,8 @@ int main()
     }
 
     swap(testArray, 2, 5);
+
+    printf("%d-------should be 1\n", indexOfMinimum(testArray, 0));
 
     while (testArray[j] != '\0') {
         printf("%d\n", testArray[j]);
